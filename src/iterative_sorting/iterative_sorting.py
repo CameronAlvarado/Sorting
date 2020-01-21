5951cf6335eda54de555df75b77bad06c4269590
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-21 14:13:55 -0800

    passed iterative sorting tests

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,23 @@
+def swap_positions(list, pos1, pos2): 
+      
+    list[pos1], list[pos2] = list[pos2], list[pos1] 
+    return list
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)): # used to be len(arr) - 1
+        # print(arr)
         cur_index = i
-        smallest_index = cur_index
+        cur_minimuim = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 l.o.c.
+        for j in range(cur_minimuim, cur_index):
+            if j < arr[0]:
+                cur_minimuim = j
 
         # TO-DO: swap
-
-
+        arr[cur_minimuim] = cur_index
 
 
     return arr
@@ -20,6 +25,17 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    arr_length = len(arr)
+    for i in range(0, arr_length):
+        # print(arr)
+        cur_index = i
+        cur_minimuim = cur_index
+        for j in range(cur_minimuim, cur_index):
+            if j < j + 1:
+                cur_minimuim = j
+
+        # TO-DO: swap
+        arr[cur_index] = cur_minimuim
 
     return arr
 
