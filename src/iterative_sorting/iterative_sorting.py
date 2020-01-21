6baaea560c01e453b949e712c3706090f3183c94
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-21 15:50:38 -0800

    realized iterative sorting code was passing tests but incorrect

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,31 +4,48 @@
 #     return list
 
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    for i in range(0, len(arr)): # used to be len(arr) - 1
-        print(arr)
-        min_index = i
-        for j in range(min_index, i):
-            if j < arr[0]:
-                min_index = j
-
-        arr[min_index] = i
-
+def selection_sort( arr ):    
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 lines of code) 
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        # TODO: swap
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
+
+# def selection_sort( arr ):
+#     for i in range(0, len(arr)):
+#         min_index = i
+#         arr[min_index] = i
+
+#     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
+
 def bubble_sort( arr ):
-    arr_length = len(arr)
-    for i in range(0, arr_length):
-        cur_minimuim = i
-        for j in range(cur_minimuim, i):
-            if j < j + 1:
-                cur_minimuim = j
-
-        arr[i] = cur_minimuim
-
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)):
+            if arr[j] > arr[i]:
+                arr[i], arr[j] = arr[j] , arr[i]
     return arr
+
+# def bubble_sort( arr ):
+#     arr_length = len(arr)
+#     for i in range(0, arr_length):
+#         cur_minimuim = i
+#         for j in range(cur_minimuim, i):
+#             if j < j + 1:
+#                 cur_minimuim = j
+
+#         arr[i] = cur_minimuim
+
+#     return arr
 
 
 # STRETCH: implement the Count Sort function below
