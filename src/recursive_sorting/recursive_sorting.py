fea9ba997c26b40765a5f1b0e1881fe09af5559f
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-21 17:39:50 -0800

    got merge and merge_sort working

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,15 +3,39 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
-    return merged_arr
+    arr = arrA + arrB
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 lines of code) 
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        # TODO: swap
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+
+
+    return arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
 
-    return arr
+    # if length of list is greater than 1,
+    # split list in half
+
+    # if length of list is equal to one, 
+    # list is sorted
+    if len(arr) <= 1:
+        return arr
+    else:
+        LHA = merge_sort(arr[:len(arr) // 2])
+        RHA = merge_sort(arr[len(arr) // 2:])
+        # merge smaller lists together into bigger lists
+        return merge(LHA, RHA)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -31,3 +55,13 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+
+# quick sort sudo:
+
+# select first index as a pivot
+# move all elements smaller than pivot to an array to the left
+# move elements greater than pivot to an array to the right
+# while <- left arr and right arr are greater than 1,
+# repeat last three steps on each arr (left and right)
